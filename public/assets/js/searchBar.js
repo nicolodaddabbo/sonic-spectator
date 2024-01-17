b7048b14e43dba510f8985d40c19fd1011ad7339
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2024-01-17 18:22:31 +0100

    [+] Enahnced search bar look ad feel

---
+++ public/assets/js/searchBar.js
@@ -0,0 +1,3 @@
+document.getElementById('searchBar').addEventListener('click', function () {
+    document.getElementById('searchInput').select();
+});
--- views/search.php
+++ views/search.php
@@ -6,14 +6,15 @@
     <?php include_once 'template/search_header.php'; ?>
     <main>
         <article class='search-content'>
-            <section class='search-bar'>
+            <section class='search-bar' id='searchBar'>
                 <img src='assets/icons/search.svg' alt='Search Icon' class='search-action-icon'>
-                <input type='text' placeholder='Search...' class='search-bar-text'>
-                <img src='assets/icons/clear.svg' alt='Clear Icon' class='search-clear-action-icon'>
+                <input type='text' placeholder='Search...' class='search-bar-text' id='searchInput'>
+                <img src='assets/icons/clear.svg' alt='Clear Icon' class='clear-action-icon'>
             </section>
         </article>
     </main>
     <?php include_once 'template/footer.php'; ?>
+    <script src="assets/js/searchBar.js"></script>
 </body>
 
 </html>
