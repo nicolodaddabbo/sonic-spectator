62556fd3c45be6a8fcd3ec9b040abf11b3ba68d6
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2024-01-17 17:51:28 +0100

    [+] Search bar in search page

--- views/search.php
+++ views/search.php
@@ -5,8 +5,12 @@
 <body>
     <?php include_once 'template/search_header.php'; ?>
     <main>
-        <article class='home-content'>
-
+        <article class='search-content'>
+            <section class='search-bar'>
+                <img src='assets/icons/search.svg' alt='Search Icon' class='search-action-icon'>
+                <input type='text' placeholder='Search...' class='search-bar-text'>
+                <img src='assets/icons/clear.svg' alt='Clear Icon' class='search-clear-action-icon'>
+            </section>
         </article>
     </main>
     <?php include_once 'template/footer.php'; ?>
