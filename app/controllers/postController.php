414ab4905c01480bc65658bfe259ff3a486d2c16
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2024-01-24 15:58:28 +0100

    [-] Unused code

--- app/controllers/postController.php
+++ app/controllers/postController.php
@@ -14,14 +14,6 @@ class PostController
         $this->postRepository = new \PostRepository();
     }
 
-    public function test(int $id, RouteCollection $routes)
-    {
-        $post = new Post();
-        $post->read($id);
-
-        require_once APP_ROOT . "/views/home.php";
-    }
-
     public function createPost(RouteCollection $routes){
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $description = $_POST['description'];
