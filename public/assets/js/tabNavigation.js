33e59b7802f07f2886297e97b75af5a663315401
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2024-02-02 11:21:52 +0100

    Navigation bar style

--- public/assets/js/tabNavigation.js
+++ public/assets/js/tabNavigation.js
@@ -1,6 +1,16 @@
 document.getElementById('homeTab').addEventListener('click', function () {
     window.location.href = '/';
 });
+// location == / 
+if (window.location.pathname == '/') {
+    document.getElementById('homeTab').classList.add('active');
+} else if (window.location.pathname == '/search') {
+    document.getElementById('searchTab').classList.add('active');
+} else if (window.location.pathname == '/post') {
+    document.getElementById('postTab').classList.add('active');
+} else if (window.location.pathname == '/profile') {
+    document.getElementById('profileTab').classList.add('active');
+}
 
 document.getElementById('searchTab').addEventListener('click', function () {
     window.location.href = 'search';
