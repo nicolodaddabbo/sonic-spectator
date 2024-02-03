03a73d64ee93889b7a3212ee7d52deca91a23398
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2024-02-03 18:52:19 +0100

    [FIX] Home accessibility

--- views/home.php
+++ views/home.php
@@ -1,18 +1,22 @@
 <?php include_once 'template/redirect.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <?php include_once 'template/head.php'; ?>
 
 <body>
     <?php include_once 'template/header.php'; ?>
+
     <main>
-        <article class='post-list'>
+        <section id='post-list' role='list'>
             <?php foreach ($posts as $post): ?>
                 <?php include 'template/post.php'; ?>
             <?php endforeach; ?>
-        </article>
+        </section>
     </main>
+
     <?php include_once 'template/footer.php'; ?>
+
     <script src="/assets/js/postActions.js"></script>
     <script src="/assets/js/notifications.js"></script>
 </body>
