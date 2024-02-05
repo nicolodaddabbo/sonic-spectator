28c7e94efd3f3dd888b0a8eceb0ad0f1dd1ca822
Nicolò D'Addabbo <nicolodaddabbo@gmail.com>
2024-02-05 18:40:08 +0100

    [FIX] Removed space between artist name and '

--- views/template/post.php
+++ views/template/post.php
@@ -3,9 +3,7 @@
         <?php if ($post['artist'] !== ''): ?>
             <section class='concert-container'>
                 <span>
-                    @
-                    <?= $post['artist'] ?>
-                    <?= substr($post['artist'], -1) === 's' ? "'" : "'s" ?> concert
+                    @ <?= $post['artist'] ?><?= substr($post['artist'], -1) === 's' ? "'" : "'s" ?> concert
                 </span>
             </section>
         <?php endif; ?>
